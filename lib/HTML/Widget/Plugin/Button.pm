3abc1e5e259d7b22a3117a446e015fd8702428db
Ricardo SIGNES <rjbs@codesimply.com>
2007-02-28 17:56:15 +0000

    more plugins

---
+++ lib/HTML/Widget/Plugin/Button.pm
@@ -0,0 +1,123 @@
+
+use strict;
+use warnings;
+
+package HTML::Widget::Plugin::Button;
+use base qw(HTML::Widget::Plugin);
+
+=head1 NAME
+
+HTML::Widget::Plugin::Button - a button for clicking
+
+=head1 VERSION
+
+version 0.001
+
+ $Id$
+
+=cut
+
+our $VERSION = '0.001';
+
+=head1 DESCRIPTION
+
+This plugin provides a basic button widget.
+
+=cut
+
+use HTML::Element;
+
+=head1 METHODS
+
+=head2 C< provided_widgets >
+
+This plugin provides the following widgets: input
+
+=cut
+
+sub provided_widgets { qw(button) }
+
+=head2 C< button >
+
+This method returns simple button element.
+
+In addition to the generic L<HTML::Widget::Plugin> attributes, the following
+are valid arguments:
+
+=over
+
+=item content
+
+This is the content of the button element, to be displayed on the button's
+face.
+
+=item type
+
+This is the type of input button to be created.  Valid types are button,
+submit, and reset.  The default is button.
+
+=item value
+
+This is the widget's initial value.
+
+=back
+
+=cut
+
+sub _attribute_args { qw(type value) }
+sub _boolean_args   { qw(disabled) }
+
+sub button {
+  my ($self, $factory, $arg) = @_;
+
+  $self->build($factory, $arg);
+}
+
+=head2 C< build >
+
+  my $widget = $class->build($factory, $arg);
+
+This method does the actual construction of the input based on the args
+collected by the widget-constructing method.  It is primarily here for
+subclasses to exploit.
+
+=cut
+
+my %TYPES = map { $_ => 1 } qw(button reset submit);
+sub __is_valid_type {
+  my ($self, $type) = @_;
+}
+
+sub build {
+  my ($self, $factory, $arg) = @_;
+
+  $arg->{attr}{name} ||= $arg->{attr}{id};
+  $arg->{attr}{type} ||= 'button';
+
+  my $widget = HTML::Element->new('button');
+
+  Carp::croak "invalid button type: $arg->{attr}{type}"
+    unless $self->__is_valid_type($arg->{attr}{type});
+
+  $widget->attr($_ => $arg->{attr}{$_}) for keys %{ $arg->{attr} };
+  $widget->push_content(
+    HTML::Element->new(
+      '~literal' => text => $arg->{content} || 'Button'
+    )
+  );
+
+  return $widget->as_XML;
+}
+
+=head1 AUTHOR
+
+Ricardo SIGNES <C<rjbs @ cpan.org>>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2007, Ricardo SIGNES.  This is free software, released under the
+same terms as perl itself.
+
+=cut
+
+1;
---
+++ lib/HTML/Widget/Plugin/Submit.pm
@@ -0,0 +1,81 @@
+
+use strict;
+use warnings;
+
+package HTML::Widget::Plugin::Submit;
+use base qw(HTML::Widget::Plugin::Input);
+
+=head1 NAME
+
+HTML::Widget::Plugin::Submit - for submit type inputs
+
+=head1 VERSION
+
+version 0.001
+
+ $Id$
+
+=cut
+
+our $VERSION = '0.001';
+
+=head1 DESCRIPTION
+
+This plugin provide a simple submit plugin, which returns an input element of
+type submit.
+
+=cut
+
+use HTML::Element;
+
+=head1 METHODS
+
+=head2 C< provided_widgets >
+
+This plugin provides the following widgets: input
+
+=cut
+
+sub provided_widgets { qw(submit) }
+
+=head2 C< submit >
+
+This method returns a basic submit input.
+
+In addition to the generic L<HTML::Widget::Plugin> attributes, the following
+are valid arguments:
+
+=over
+
+=item value
+
+This is the widget's initial value, which (for submit inputs) is generally used
+as the text for the label on its face.
+
+=back
+
+=cut
+
+sub submit {
+  my ($self, $factory, $arg) = @_;
+
+  $arg->{attr}{type} ||= 'submit';
+
+  # I suppose I could carp, here, if the type is altered, but... it's your
+  # foot, shoot it if you want. -- rjbs, 2007-02-28]
+
+  $self->build($factory, $arg);
+}
+
+=head1 AUTHOR
+
+Ricardo SIGNES <C<rjbs @ cpan.org>>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2007, Ricardo SIGNES.  This is free software, released under the
+same terms as perl itself.
+
+=cut
+
+1;
